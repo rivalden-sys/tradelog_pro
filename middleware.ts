9291e27b36fc6f5c716bf20b7eb72f975e8bb8e9
rivalden-sys <denis777novikov@gmail.com>
2026-04-18 14:19:15 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
-import { type NextRequest, NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/lib/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-  return NextResponse.next()
+  return await updateSession(request)
 }
 
 export const config = {
-  matcher: [],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 }
